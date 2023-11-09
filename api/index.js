70a05a0890f4cffabed7467f98a2add85d919232
Tammy <tammyslau29@gmail.com>
2023-11-08 20:35:13 -0800

    create connection to MongoDB

--- api/index.js
+++ api/index.js
@@ -1,5 +1,14 @@
 import express from "express"
- 
+import mongoose from "mongoose";
+import dotenv from "dotenv"
+
+dotenv.config()
+mongoose.connect(process.env.MONGO_DB).then(() =>{
+    console.log("Connected to MongoDB ✨")
+}).catch((err) => {
+    console.log("Error connecting to MongoDB", err)
+})
+
 const app = express();
 
 app.listen(3000, () => {
